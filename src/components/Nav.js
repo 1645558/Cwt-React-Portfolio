d38404ed63c15be01060f3f7a469e46a3277c51f
Rare Raptor <connorwtuncc@gmail.com>
2022-07-25 23:16:36 -0400

    added pages, got the basics laid out to display headers, added styling to home page and some filler text

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/Navbar.css'
 
 function Nav({ setPage }) {
   return (
-    <div>
+    <div className='navbar'>
       <a href="#portfolio" onClick={() => setPage('portfolio')}>Portfolio</a>
       <a href="#about" onClick={() => setPage('about')}>About</a>
       <a href="#contact" onClick={() => setPage('contact')}>Contact</a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <>
+    <header className='header'>
+    <h1>Connor Thompson</h1>
+    </header>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto ab molestiae labore consequuntur tempore nulla magnam possimus facere unde maiores aut sint modi distinctio, et autem commodi perspiciatis aspernatur.</p>
+    </>
   )
 }
 
