e33124588d6e6b0e7009f7163cdfde862cd4a099
Rare Raptor <connorwtuncc@gmail.com>
2022-07-26 20:57:45 -0400

    got contact form done, added inputs and form control/validation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Contact.css'
 
 function Contact() {
+
+  const [names, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'message') {
+      setMessage(inputValue)
+    }
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!names || !email) {
+      setErrorMessage('Name or Email is invalid');
+      return;
+    }
+
+    setName('');
+    setEmail('');
+    setMessage('');
+
+  }
+
   return (
     <>
       <header className='header'>
         <h1>Contact Me</h1>
       </header>
       <section className='section'>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
 
+        <form className='form'>
+          <input
+            value={names}
+            onChange={handleInputChange}
+            type="text"
+            name="name"
+            placeholder='Name' />
+
+          <input
+            value={email}
+            onChange={handleInputChange}
+            type="text"
+            name="email"
+            placeholder='Email' />
+
+          <input 
+          value={message}
+          onChange={handleInputChange}
+          type="text" 
+          name="message" 
+          placeholder='Message' />
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
       </section>
     </>
   )
