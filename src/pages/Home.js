5e9db11e11dfc7d4a2832a51314fae2d2c7d037d
Rare Raptor <connorwtuncc@gmail.com>
2022-07-26 22:22:05 -0400

    having trouble getting font awesome to work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import '../styles/Home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-core'
+// import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Home() {
   return (
@@ -10,6 +13,10 @@ function Home() {
     <section className='section'>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto ab molestiae labore consequuntur tempore nulla magnam possimus facere unde maiores aut sint modi distinctio, et autem commodi perspiciatis aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto ab molestiae labore consequuntur tempore nulla magnam possimus facere unde maiores aut sint modi distinctio, et autem commodi perspiciatis aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto ab molestiae labore consequuntur tempore nulla magnam possimus facere unde maiores aut sint modi distinctio, et autem commodi perspiciatis aspernatur.</p>
     </section>
+    <i class="fa-solid fa-user"></i>
+    <div className='flex justify-center'>
+    {/* <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> */}
+    </div>
     </>
   )
 }
