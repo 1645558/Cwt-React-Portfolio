86d4fc2a551781bacc5014942b1f0356b025e4aa
Rare Raptor <connorwtuncc@gmail.com>
2022-07-26 21:45:48 -0400

    got download resume figured out

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,6 +8,7 @@ function Resume() {
     <h1>Resume</h1>
     </header>
     <section className='section'>
+      <a href={require('../resume/Connor-Thompson.pdf')} download>Download</a>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto ab molestiae labore consequuntur tempore nulla magnam possimus facere unde maiores aut sint modi distinctio, et autem commodi perspiciatis aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto ab molestiae labore consequuntur tempore nulla magnam possimus facere unde maiores aut sint modi distinctio, et autem commodi perspiciatis aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto ab molestiae labore consequuntur tempore nulla magnam possimus facere unde maiores aut sint modi distinctio, et autem commodi perspiciatis aspernatur.</p>
     </section>
     </>
